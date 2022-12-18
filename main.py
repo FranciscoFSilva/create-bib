6b597931bf061d1395922e08263f394c3bcf74c6
FranciscoFSilva <francisco.silva.e@gmail.com>
2022-12-18 21:07:06 +0000

    created functions to get title, first author and date (year-month-day). Printed to see if correct. Everything working fine

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from habanero import Crossref, WorksContainer
 from argparse import ArgumentParser
 
@@ -7,6 +8,10 @@ def get_work(cr,doi,sort = None):
 def get_authors(work):
     x = WorksContainer(work)
     return x.author[0]
+
+def get_title(work):
+    x = WorksContainer(work)
+    return x.title[0]
 
 def get_authors_string(authors):
     noAuthors = len(authors)
@@ -24,6 +29,21 @@ def get_authors_string(authors):
         idx +=2
     return stringList
 
+def get_first_author(authors):
+    for author in authors:
+        if "sequence" in author:
+            if author["sequence"] == "first":
+                return author["family"]
+    return ""
+
+def get_published_date(work):
+    x = WorksContainer(work)
+    if hasattr(x, "published_print"):
+        published_print = x.published_print
+        date = published_print[0]
+        return date["date-parts"][0]
+    return []
+
 if __name__ == "__main__":
     mailto = "francisco.ferreira.silva@tecnico.ulisboa.pt"
     cr = Crossref(mailto = mailto)
@@ -36,6 +56,13 @@ if __name__ == "__main__":
         data_list = data.split("\n")
         works = get_work(cr,data_list)
         for work in works:
+            date = get_published_date(work)
             authors = get_authors(work)
+            title = get_title(work)
+            first = get_first_author(authors)
             authors_string = get_authors_string(authors)
-            print(" ".join(authors_string))
+            authors_string = get_authors_string(authors)
+            if not date:
+                date = [0]
+            text = f"{' '.join(map(str,date))} - {' '.join(authors_string)}: {title[0]}."
+            print(text)
